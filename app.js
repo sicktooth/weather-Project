2df6616db014e2601fccae3779ee09128c9e7043
sicktooth <sosaronungwe@gmail.com>
2024-06-25 05:25:56 +0100

    learning about parsing JSON

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 import express from "express";
-import path from "path";
+import { log } from "node:console";
+// import path from "path";
+import https from "node:https";
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Port Harcourt&units=metric&appid=340e6b918ec7d7dd6fb1b86e6c23bb5c";
+
+  https.get(url, (response) => {
+    console.log(response.statusCode);
+
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data)
+      const weatherTemp = weatherData.main.temp,
+            weatherDescription = weatherData.weather[0].description;
+
+      log(weatherData);
+      log(weatherTemp);
+      log(weatherDescription);
+    });
+    
+  });
+
   res.send("Hello World!");
 });
 
