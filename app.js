67efa8142c6e40699f3142bd292ac9e557edfa83
sicktooth <sosaronungwe@gmail.com>
2024-08-25 22:43:11 +0100

    refactor code for ejs template

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-import express from "express";
-import { log } from "node:console";
-import path from "path";
-import https from "node:https";
+const express = require("express");
+// import { log } from "node:console";
+// import path from "path";
+const https = require("https");
+// import https from "node:https";
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
+var weather = [];
+
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  // res.sendFile(path.join(__dirname, "public", "index.html"));
+
+  res.render("list", { 
+    city: weather[3],
+    temp: weather[1],
+    description: weather[2],
+    weatherIcon: weather[0]
+   });
 });
 
 app.post("/", (req, res) => {
@@ -30,15 +41,20 @@ app.post("/", (req, res) => {
             weatherIcon = weatherData.weather[0].icon,
             iconUrl = "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
 
-      res.write(`<p>The weather is currently ${weatherDescription}</p>`);
-      res.write(`<h1>The temperature in ${query} is ${weatherTemp} degrees Celsius</h1>`);
-      res.write(`<img src="${iconUrl}">`);
-      res.send();
+      // res.write(`<p>The weather is currently ${weatherDescription}</p>`);
+      // res.write(`<h1>The temperature in ${query} is ${weatherTemp} degrees Celsius</h1>`);
+      // res.write(`<img src="${iconUrl}">`);
+      // res.send();
+      
+      weather.push(iconUrl, weatherTemp, weatherDescription, query);
+      
+      console.log(weather);
+      res.redirect("/");
     });
     
   });
 });
 
 app.listen(port, () => {
-  log(`weather app server has started at http://localhost:${port}`);
+  console.log(`weather app server has started at http://localhost:${port}`);
 });
